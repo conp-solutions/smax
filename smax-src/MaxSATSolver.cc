0aeee5f894a3125b59b95c403ed246dcb10ca4a0
Norbert Manthey <nmanthey@conp-solutions.com>
2018-03-11 22:48:47 +0100

    smax: implement first basic functions

--- smax-src/MaxSATSolver.cc
+++ smax-src/MaxSATSolver.cc
@@ -8,46 +8,106 @@ Copyright (c) 2017, Norbert Manthey, all rights reserved.
 #include <vector>
 
 #include <errno.h>
-
+#include "smax-src/SMaxLimits.h"
 #include "include/MaxSATSolver.h"
+#include "smax-src/SMaxSolver.h"
 
-
+using namespace SMax;
 
 const char* MaxSATSolver::getSolverName () const
 {
-	return "smax - Norbert Manthey, 2018";
+  return "smax - Norbert Manthey, 2018";
 }
 
 MaxSATSolver::MaxSATSolver(int nVars, int nClausesEstimate)
 {
-  
+  // try to run usual code, but catch exceptions if they happen
+  try {
+    // fail if the input values are too large, results in errno -ENOMEM
+    if(nVars >= SMAX_MAX_VAR ) return;
+    if(nClausesEstimate >= SMAX_MAX_CLS ) return;
+
+    externalData = new SMax::SMaxSolver(nVars);
+    if(!externalData) return; // results in -ENOMEM
+    SMaxSolver *solver = (SMaxSolver*)externalData;
+    
+    solver->reserveVars(solver->nVars());
+    while ((int)nVars > solver->nVars()) solver->newVar();
+  }
+  catch (...)
+  {
+    // in case we created an object, kill it again, and set -ENOMEM
+    if(externalData) {
+      delete (SMaxSolver*)externalData;
+      externalData = nullptr; 
+    }
+  }
 }
     
 MaxSATSolver::~MaxSATSolver()
 {
-  
+  if(externalData) delete (SMaxSolver*)externalData;
+  externalData = nullptr;
 }
 
-    int MaxSATSolver::getErrno() const
-    {
-      return -ENOMEM;
-    }
+int MaxSATSolver::getErrno() const
+{
+  if(!externalData) return -ENOMEM;
+  SMaxSolver *solver = (SMaxSolver*)externalData;
+  return solver->getErrno();
+}
 
-    bool MaxSATSolver::addClause(const std::vector<int> &literals, uint64_t weight)
-    {
-	return false; // not yet implemented
-    }
+bool MaxSATSolver::addClause(const std::vector<int> &literals, uint64_t weight)
+{
+  if(!externalData) return false;
+  SMaxSolver *solver = (SMaxSolver*)externalData;
+  if(solver->getErrno() != 0 && solver->getErrno() != -EINVAL) return false;
+  
+  if(weight > SMOOTHER_MAX_WEIGHT) {
+    solver->setErrno(-EINVAL);
+    return false;
+  }
+  
+  solver->setErrno(0);
 
-    bool MaxSATSolver::addAtMostK(const std::vector<int> &literals, const unsigned k)
-    {
-	return false; // not yet implemented      
-    }
+  try {
+    bool ret = solver->addClause(literals, weight);
+    if(!ret) solver->setErrno(-EINVAL);
+    return ret;
+  }
+  catch (...)
+  {
+    // in case of the exception, we assume insufficient memory
+    solver->setErrno(-ENOMEM);
+    return ERROR;
+  }
+}
+
+bool MaxSATSolver::addAtMostK(const std::vector<int> &literals, const unsigned k)
+{
+  if(!externalData) return false;
+  SMaxSolver *solver = (SMaxSolver*)externalData;
+  if(solver->getErrno() != 0 && solver->getErrno() != -EINVAL) return false;
+  solver->setErrno(0);
+
+  try {
+    bool ret = solver->addAtMostK(literals, k);
+    if(!ret) solver->setErrno(-EINVAL);
+    return ret;
+  }
+  catch (...)
+  {
+    // in case of the exception, we assume insufficient memory
+    solver->setErrno(-ENOMEM);
+    return ERROR;
+  }
+}
     
-    MaxSATSolver::ReturnCode MaxSATSolver::compute_maxsat(std::vector<int> &model,
+MaxSATSolver::ReturnCode MaxSATSolver::compute_maxsat(std::vector<int> &model,
                               uint64_t maxCost,
                               const std::vector<int> *startAssignment,
                               int maxMinimizeSteps)
-    {
-      return MaxSATSolver::ReturnCode::UNKNOWN; // not yet implemented
-    }
+{
+  return MaxSATSolver::ReturnCode::UNKNOWN; // not yet implemented
+}
 
