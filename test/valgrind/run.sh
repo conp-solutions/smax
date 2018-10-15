44599771e597210aa1aab4962c96d916b648e4e6
Norbert Manthey <nmanthey@conp-solutions.com>
2018-10-15 22:32:41 +0200

    test: extend valgrind analysis

--- test/valgrind/run.sh
+++ test/valgrind/run.sh
@@ -10,7 +10,7 @@ set -e -x
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # build the debug version
-if [ -d "$script_dir"/../../smax-src ]
+if [ -d "$script_dir"/../../smax-src ] && [ -z "$VALGRIND_NO_REBUILD" ]
 then
 	pushd "$script_dir"/../../smax-src
 	make clean
@@ -18,14 +18,15 @@ then
 	popd
 fi
 
-# make sure we are working where the cnfs are located
-pushd "$script_dir"
+CHECK_STATUS=0
 
-# start fresh
-rm -f *.log
+# make sure we are working where the cnfs are located
+cd "$script_dir"
+
+BENCHMARKS=$(ls benchmarks/*cnf || true)
 
 # let's iterate over all files
-for file in *.cnf
+for file in *.cnf $BENCHMARKS
 do
 
 	echo "check file $file"
@@ -34,7 +35,6 @@ do
 	# check single core, and disjunct cores
 	for parameter in ""
 	do
-
 		LOGFILE="$b-$parameter.log"
 
 		# run tool via valgrind
@@ -43,16 +43,19 @@ do
 			--leak-check=full \
 			--show-leak-kinds=all \
 			"$script_dir"/../../smax-src/smax_debug \
-				$parameter \
+				-verb=2 $parameter \
 				$file |& tee "$LOGFILE"
 
 		# make sure we do not have any errors
-		grep "ERROR SUMMARY: 0 errors" "$LOGFILE" || exit 1
+		grep "ERROR SUMMARY: 0 errors" "$LOGFILE" || CHECK_STATUS=1
 
+		echo "for 2 $CHECK_STATUS"
+		[ $CHECK_STATUS -eq 0 ] || break
 	done
-done
+	echo "for 1 $CHECK_STATUS"
+	[ $CHECK_STATUS -eq 0 ] || break
+
+done | tee valgrind-benchmark.log
 
 # return back to calling directory
-popd
-
-echo "SUCCESS!"
+cd -
