5f0a075bfa1122f1c7675461b86cde3f9ac07086
Norbert Manthey <nmanthey@conp-solutions.com>
2019-07-07 22:01:34 +0200

    src: add make defines
    The provided files are compiled in a pre-defined way. To be able to use
    the library when linking to other tools, make sure these flags are
    available.

---
+++ smax-src/common.mk
@@ -0,0 +1,12 @@
+# Build flags to be used when building files or projects
+
+# Optimize the code as much as possible
+CFLAGS    += -flto
+
+CFLAGS     += -Wextra -Wno-unused-parameter
+
+# Make sure we do not introduce warnings again
+CFLAGS     += -Wno-literal-suffix -Werror
+
+# Make sure we can handle flto
+AR = gcc-ar
