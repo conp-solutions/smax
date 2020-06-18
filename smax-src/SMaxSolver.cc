conp-solutions/smax

commit 0ae859943084bb98d6145c76c5b759616b49e28d

unsat: return unsat in case the optimal value is above top
Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

smax-src/SMaxSolver.cc
@@ -431,12 +431,17 @@ MaxSATSolver::ReturnCode SMaxSolver::compute_maxsat(vector< int >& model, uint64
       delete S; S = nullptr;
       return MaxSATSolver::ReturnCode::SATISFIABLE;
     case _OPTIMUM_:
-      cost = S->getLastBound();
-      model.resize(inputVarCnt + 1, 0);
-      for(Var v = 0; v < inputVarCnt; ++ v)
-	model[v+1] = S->getValue(v);
-      delete S; S = nullptr;
-      return MaxSATSolver::ReturnCode::OPTIMAL;
+      if(maxCost < cost) {
+          delete S; S = nullptr;
+          return MaxSATSolver::ReturnCode::UNSATISFIABLE;
+      } else {
+          cost = S->getLastBound();
+          model.resize(inputVarCnt + 1, 0);
+          for(Var v = 0; v < inputVarCnt; ++ v)
+              model[v+1] = S->getValue(v);
+          delete S; S = nullptr;
+          return MaxSATSolver::ReturnCode::OPTIMAL;
+      }
     default:
       delete S; S = nullptr;
       return MaxSATSolver::UNKNOWN;

commit a3fd3f6de1d5a5025d96ea8c6cff1af3a2092279

optimum: fix extracting bound
While reworking printing the output of the solver, handling the bound was
moved into an if statement, leading to return UNSAT for all optimal
answers. This change pulls the extraction out again.

Fixes: 0ae8599 (unsat: return unsat in case the optimal value is above top)

Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

smax-src/SMaxSolver.cc
@@ -436,11 +436,11 @@ MaxSATSolver::ReturnCode SMaxSolver::compute_maxsat(vector< int >& model, uint64
           return MaxSATSolver::ReturnCode::SATISFIABLE;
       }
     case _OPTIMUM_:
+      cost = S->getLastBound();
       if(maxCost < cost) {
           delete S; S = nullptr;
           return MaxSATSolver::ReturnCode::UNSATISFIABLE;
       } else {
-          cost = S->getLastBound();
           model.resize(inputVarCnt + 1, 0);
           for(Var v = 0; v < inputVarCnt; ++ v)
               model[v+1] = S->getValue(v);